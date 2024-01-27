cf0a6ee953fc4f70854b86a2f5b5a77ea1f2dc8e
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-27 19:58:50 +0530

    score

--- src/GetLiveScore.jsx
+++ src/GetLiveScore.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const GetLiveScore = () => {
   const socket = io("http://localhost:5000/"); // Connect to the server
-  const [score, setscore] = useState(0)
+  const [score, setscore] = useState([]);
   useEffect(() => {
     console.log("useEffect called");
     socket.on("updateScore", (data) => {
-      console.log("data in live score...",data);
-    })
+      console.log("data in live score...", data);
+      console.log("score...", score);
+      setscore([...score, data]);
+
+    });
     socket.on("noti", (data) => {
       toast(data, {
         position: "bottom-left",
@@ -21,28 +24,33 @@ export const GetLiveScore = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
-    })
-  }, []);
+      });
+    });
+  }, [score]);
 
   return (
     <div>
-      
       <h1>live score</h1>
+      <ul>
+        {
+          score.map((item, index) => {
+            return <li key={index}>{item.playerName} {item.score}</li>
+          })
+        }
+      </ul>
 
       <ToastContainer
-position="bottom-left"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"/>
-
-      
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };
